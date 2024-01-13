dd935f05fcf85e97499153a0beb5a01da320ed00
GabrielBoos22 <gabriel.bduarte@sptech.school>
2024-01-13 02:30:58 -0300

    Cadastro topico com dto

---
+++ projeto/src/main/kotlin/br/com/alura/forum/dto/NovoTopicoDto.kt
@@ -0,0 +1,9 @@
+package br.com.alura.forum.dto
+
+data class NovoTopicoDto(
+    val titulo: String,
+    val mensagem: String,
+    val idCurso: Long,
+    val idUsuario: Long,
+)
+
--- projeto/src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
+++ projeto/src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
@@ -1,62 +1,16 @@
 package br.com.alura.forum.service
 
+import br.com.alura.forum.dto.NovoTopicoDto
 import br.com.alura.forum.model.Curso
 import br.com.alura.forum.model.Topico
 import br.com.alura.forum.model.Usuario
 import org.springframework.stereotype.Service
 
 @Service
-class TopicoService (private var topicos: List<Topico>){
-    init {
-        val topico = Topico(
-            id = 1,
-            titulo = "Duvida Kotlin",
-            mensagem = "Variaveis no Kotlin",
-            curso = Curso (
-                id = 1,
-                nome = "Kotlin",
-                categoria = "Programacao"
-            ),
-            autor = Usuario(
-                id = 1,
-                nome = "Ana da Silva",
-                email = "ana@email.com"
-            )
-        )
-        val topico2 = Topico(
-            id = 2,
-            titulo = "Duvida Kotlin 2",
-            mensagem = "Variaveis no Kotlin 2",
-            curso = Curso (
-                id = 1,
-                nome = "Kotlin",
-                categoria = "Programacao"
-            ),
-            autor = Usuario(
-                id = 1,
-                nome = "Ana da Silva",
-                email = "ana@email.com"
-            )
-        )
-        val topico3 = Topico(
-            id = 3,
-            titulo = "Duvida Kotlin 3",
-            mensagem = "Variaveis no Kotlin 3",
-            curso = Curso (
-                id = 1,
-                nome = "Kotlin",
-                categoria = "Programacao"
-            ),
-            autor = Usuario(
-                id = 1,
-                nome = "Ana da Silva",
-                email = "ana@email.com"
-            )
-        )
-
-        topicos = listOf(topico, topico2, topico3)
-    }
-
+class TopicoService (
+    private var topicos: List<Topico> = ArrayList(),
+    private var cursoService: CursoService,
+    private var usuarioService: UsuarioService){
     fun listar(): List<Topico>{
         return topicos
     }
@@ -65,5 +19,16 @@ class TopicoService (private var topicos: List<Topico>){
         return topicos.stream().filter({
                 t -> t.id == id
         }).findFirst().get()
+    }
+
+    fun cadastrar(dto: NovoTopicoDto) {
+
+        topicos = topicos.plus(Topico(
+            id = topicos.size.toLong() + 1,
+            titulo = dto.titulo,
+            mensagem = dto.mensagem,
+            curso = cursoService.buscarPorId(dto.idCurso),
+            autor = usuarioService.buscarPorId(dto.idUsuario)
+        ))
     }
 }
